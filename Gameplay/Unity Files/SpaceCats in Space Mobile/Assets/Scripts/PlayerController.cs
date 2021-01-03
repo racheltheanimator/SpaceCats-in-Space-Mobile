5eb20574e3d25a8d641f151cdabf80f3fbd2b4ba
racheltheanimator <racheltheanimator@gmail.com>
2021-01-03 16:08:29 -0500

    Add new unity poject

---
+++ Gameplay/Unity Files/SpaceCats in Space Mobile/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //movement variables
+
+    public float maxSpeed;
+
+    public Rigidbody2D myRB;
+
+    Vector2 movement;
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+
+    void FixedUpdate()
+    {
+
+
+        myRB.MovePosition(myRB.position + movement * maxSpeed * Time.fixedDeltaTime);
+    }
+}
